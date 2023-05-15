d5abf68ea3bfdeb01c5e06256c56d14faa7362a9
Henok <hennok100@gmail.com>
2023-05-15 15:59:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Footer />
+      <Services />
+      <Tours />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/note.js
@@ -0,0 +1,3 @@
+/**
+ we use key in a list when we try to render a list of things
+ */
